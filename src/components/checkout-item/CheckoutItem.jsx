ed95ab1512bf4e896309777ee323acf834ce3bd0
Christine Fong <christinefongwl@gmail.com>
2021-03-19 22:54:49 +0800

    finished Checkout Page and modified some styling

---
+++ src/components/checkout-item/CheckoutItem.jsx
@@ -0,0 +1,46 @@
+import { connect } from 'react-redux';
+
+import { addItem, removeItem, clearItemFromCart } from '../../redux/cart/cart-actions';
+
+import { ReactComponent as LeftIcon } from '../../assets/left-icon.svg';
+import { ReactComponent as RightIcon } from '../../assets/right-icon.svg';
+import { ReactComponent as ClearIcon } from '../../assets/clear-icon.svg';
+
+import styles from './checkout-item.module.scss';
+
+const CheckoutItem = ({ cartItem, addItem, removeItem, clearItemFromCart }) => {
+  const { imageUrl, name, quantity, price } = cartItem;
+  
+  return (
+    <div className={styles.checkoutItem}>
+      <div className={styles.image}>
+        <img src={imageUrl} alt='item' />
+        </div>
+      <div className={styles.name}>
+        <span>{name}</span>
+      </div>
+      <div className={styles.price}>
+        <span>NT${price}</span>
+      </div>
+      <div className={styles.quantity}>
+        <LeftIcon onClick={() => removeItem(cartItem)} />
+        <div><span>{quantity}</span></div>
+        <RightIcon onClick={() => addItem(cartItem)}/>
+      </div>
+      <div className={styles.total}>
+        <span>NT${price * quantity}</span>
+      </div>
+      <div className={styles.removeButton} onClick={() => clearItemFromCart(cartItem)}>
+        <ClearIcon />
+      </div>
+    </div>
+  )
+};
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item)),
+  clearItemFromCart: item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/checkout-page/CheckoutPage.jsx
+++ src/pages/checkout-page/CheckoutPage.jsx
@@ -1,11 +1,45 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { Link } from 'react-router-dom';
+
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart-selectors';
+
 import Breadcrumb from '../../components/breadcrumb/Breadcrumb';
+import CheckoutItem from '../../components/checkout-item/CheckoutItem';
+import CustomButton from '../../components/custom-button/CustomButton';
+
 import styles from './checkout-page.module.scss';
 
-const CheckoutPage = ({ location }) => (
+const CheckoutPage = ({ location, cartItems, total }) => (
   <div className={styles.checkoutPage}>
     <Breadcrumb location={location}/>
-    Checkout Page
+    <h1>我的購物袋</h1>
+    {cartItems.length ? 
+      <div className={styles.withCartItems}>
+        <div className={styles.checkoutHeader}>
+          <span>商品</span>
+          <span>單價</span>
+          <span>數量</span>
+          <span>總金額</span>
+        </div>
+        {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)}
+        <div className={styles.totalGroup}>
+          <span className={styles.note}>小計:</span>
+          <span className={styles.total}>NT${total}</span>
+        </div>
+      </div>
+      :
+      <div className={styles.withoutCartItems}>
+       <h2>目前您的購物車沒有商品，請點選前往購物，開始選購商品</h2>
+       <Link to='/shop'><CustomButton>開始購物</CustomButton></Link>
+      </div>
+    }
   </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  total: selectCartTotal
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
