0fc0d73ff728b53e070a539475073c298ade612d
Christine Fong <christinefongwl@gmail.com>
2021-03-25 22:34:56 +0800

    modified My Wishlist Page

--- src/pages/my-wishlist-page/MyWishlistPage.jsx
+++ src/pages/my-wishlist-page/MyWishlistPage.jsx
@@ -10,10 +10,16 @@ import styles from './my-wishlist-page.module.scss';
 const MyWishlistPage = ({ wishlistItems }) => (
   <div className={styles.myWishlistPage}>
     <h1>我的願望清單</h1>
-    <h2>{wishlistItems.length}件商品</h2>
-    <div className={styles.wishlistItems}>
-      {wishlistItems.map(wishlistItem => <WishlistItem wishlistItem={wishlistItem} />)}
-    </div>
+    {wishlistItems.length ?
+      <div>
+        <h2>{wishlistItems.length}件商品</h2>
+        <div className={styles.wishlistItems}>
+          {wishlistItems.map(wishlistItem => <WishlistItem wishlistItem={wishlistItem} />)}
+        </div>
+      </div>
+      :
+      <h2>你沒有加入商品到願望清單</h2>
+    }
   </div>
 );
 
