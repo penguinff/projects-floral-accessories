9d912abb1c96efc01007ed19408bb5d19484956c
Christine Fong <christinefongwl@gmail.com>
2021-03-26 19:48:33 +0800

    amended the sale message

--- src/components/sale-message/SaleMessage.jsx
+++ src/components/sale-message/SaleMessage.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as ClearIcon } from '../../assets/clear-icon.svg';
 const SaleMessage = () => (
   <div className={styles.saleMessage}>
     <span className={styles.messageWord}>
-      只有三天 ♡ 全館滿1500折100
+      只有三天 ♡ 全館滿千免運費
     </span>
     <span className={styles.messageClear}>
       <ClearIcon />
