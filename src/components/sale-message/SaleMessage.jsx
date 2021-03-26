5dc11e903bc618941e9decd4b6f3437398c0944e
Christine Fong <christinefongwl@gmail.com>
2021-03-26 20:55:17 +0800

    added hidden function to sale message

--- src/components/sale-message/SaleMessage.jsx
+++ src/components/sale-message/SaleMessage.jsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
+
 import styles from './sale-message.module.scss';
 
 import { ReactComponent as ClearIcon } from '../../assets/clear-icon.svg';
 
-const SaleMessage = () => (
-  <div className={styles.saleMessage}>
-    <span className={styles.messageWord}>
-      只有三天 ♡ 全館滿千免運費
-    </span>
-    <span className={styles.messageClear}>
-      <ClearIcon />
-    </span>
-  </div>
-);
+const SaleMessage = () => {
+  const [hiddenSaleMessage, setHiddenSaleMessage] = useState(false);
+
+  return (
+    <div className={styles.saleMessage} hidden={hiddenSaleMessage}>
+      <span className={styles.messageWord}>
+        只有三天 ♡ 全館滿千免運費
+      </span>
+      <span className={styles.messageClear}>
+        <ClearIcon onClick={() => setHiddenSaleMessage(true)} />
+      </span>
+    </div>
+  )
+};
 
 export default SaleMessage;
