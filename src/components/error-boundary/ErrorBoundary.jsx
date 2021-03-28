bd7da50179420f077b9aebdb4583d1911936c47a
Christine Fong <christinefongwl@gmail.com>
2021-03-28 15:45:13 +0800

    modified font-size in media query

---
+++ src/components/error-boundary/ErrorBoundary.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+import styles from './error-boundary.module.scss';
+
+const ErrorBoundary = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default ErrorBoundary;
