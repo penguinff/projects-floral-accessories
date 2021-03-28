d44080cd98d8fb880f6bafa8734de9e0039efec4
Christine Fong <christinefongwl@gmail.com>
2021-03-28 21:05:39 +0800

    removed unused folders

--- src/components/error-boundary/ErrorBoundary.jsx
+++ src/components/error-boundary/ErrorBoundary.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import CustomButton from '../custom-button/CustomButton';
 import ErrorMessage from '../error-message/ErrorMessage';
-
-import styles from './error-boundary.module.scss';
-
-import { ReactComponent as LeafGreenSpinner } from '../../assets/leaf-green-spinner.svg';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
