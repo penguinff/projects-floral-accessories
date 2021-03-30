629b77898d0a15a872932630f9d43f2aeb759eb7
Christine Fong <christinefongwl@gmail.com>
2021-03-30 16:03:42 +0800

    modified styling of product images

--- src/pages/product-page/ProductPage.jsx
+++ src/pages/product-page/ProductPage.jsx
@@ -80,7 +80,9 @@ const ProductPage = ({ match, location, history, product, wishlistItems, addItem
           <span>Product Details</span>
         </div>
         <div className={styles.details}>
-          <img src={imageUrl} alt='product image' />
+          <div className={styles.images}>
+            <img src={imageUrl} alt='product image' />
+          </div>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
