a2b84acac02b53ba2285a98439d33f72e005e3cb
Christine Fong <christinefongwl@gmail.com>
2021-07-17 16:03:00 +0800

    modified some styling

--- src/components/side-nav/SideNav.jsx
+++ src/components/side-nav/SideNav.jsx
@@ -9,18 +9,22 @@ import { ReactComponent as ArrowDownIcon } from '../../assets/arrow-down-icon.sv
 import { ReactComponent as ArrowUpIcon } from '../../assets/arrow-up-icon.svg';
 
 const SideNav = ({ showSideNav, setShowSideNav }) => {
+  // local state for displaying category
   const [showCategory, setShowCategory] = useState(false);
 
-  const toggleSideNav = () => setShowSideNav(!showSideNav);
-  const toggleCategory = () => setShowCategory(!showCategory);
+  // set toggle functions
+  const toggleSideNav = () => setShowSideNav(showSideNav => !showSideNav);
+  const toggleCategory = () => setShowCategory(showCategory => !showCategory);
 
   return (
     <nav className={`${styles.sideNav} ${showSideNav && styles.sideNavActive}`}>
+
       <div className={styles.content}>
-        <ul className={styles.upper} onClick={toggleSideNav}>
-          <li><ClearIcon/>目錄</li>
-          <Link to='/user-profile'><li><UserIcon />我的賬戶</li></Link>
+        <ul className={styles.upper}>
+          <li onClick={toggleSideNav}><ClearIcon/>目錄</li>
+          <Link to='/user-profile' onClick={toggleSideNav}><li><UserIcon />我的賬戶</li></Link>
         </ul>
+
         <ul className={styles.middle}>
           <Link to='/shop/new-arrival' onClick={toggleSideNav}><li>新品上市</li></Link>
           <li onClick={toggleCategory} className={styles.title}>
@@ -31,7 +35,8 @@ const SideNav = ({ showSideNav, setShowSideNav }) => {
           </li>
           <ul 
             className={styles.category}
-            hidden={!showCategory} onClick={() => {toggleSideNav(); toggleCategory()}}
+            hidden={!showCategory} 
+            onClick={toggleSideNav}
           >
             <Link to='/shop/earrings'><li>耳環</li></Link>
             <Link to='/shop/necklaces'><li>項鏈</li></Link>
@@ -43,12 +48,15 @@ const SideNav = ({ showSideNav, setShowSideNav }) => {
           <Link to='/under-construction' onClick={toggleSideNav}><li>會員專區</li></Link>
           <Link to='/under-construction' onClick={toggleSideNav}><li>潮流話題</li></Link>
         </ul>
-        <ul className={styles.lower}onClick={toggleSideNav}>
-          <Link to='/about-us' onClick={toggleSideNav}><li>關於 Floral Accessories</li></Link>
+
+        <ul className={styles.lower} onClick={toggleSideNav}>
+          <Link to='/about-us'><li>關於 Floral Accessories</li></Link>
           <Link to='/contact-us'><li>聯絡我們</li></Link>
         </ul>
       </div>
+
       <div className={styles.background} onClick={toggleSideNav}></div>
+
     </nav>
   )
 };
